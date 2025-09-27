318eab8616d4063817a0d801679bd44f1ee225a1
farrel irawan <irawanssfarrel@gmail.com>
2025-09-27 16:49:42 +0700

    add temperature conversion function from Celsius to Fahrenheit

---
+++ main.py
@@ -0,0 +1,5 @@
+# python temperature convertion program
+
+def celcius_to_fahrenheit(temperature_celcius):
+    temperature_fahrenheit = (temperature_celcius * 9/5) + 32
+    return temperature_fahrenheit
