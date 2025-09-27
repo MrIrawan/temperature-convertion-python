238f8906ea439cd91d75fdaf15517df8ffafb33f
farrel irawan <irawanssfarrel@gmail.com>
2025-09-27 16:56:00 +0700

    add Celsius to Reamur conversion function

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def celcius_to_fahrenheit(temperature_celcius):
     temperature_fahrenheit = (temperature_celcius * 9/5) + 32
     return temperature_fahrenheit
 
+def celcius_to_reamur(temperature_celcius):
+    temperature_reamur = temperature_celcius * 4/5
+    return temperature_reamur
+
 def fahrenheit_to_celcius(temperature_fahrenheit):
     temperature_celcius = (temperature_fahrenheit - 32) * 5/9
     return temperature_celcius
