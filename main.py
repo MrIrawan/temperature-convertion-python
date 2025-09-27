58a85aedba67df9a84f965e958b29462c28a258e
farrel irawan <irawanssfarrel@gmail.com>
2025-09-27 16:52:38 +0700

    add fahrenheit to Celsius conversion function

--- main.py
+++ main.py
@@ -3,3 +3,7 @@
 def celcius_to_fahrenheit(temperature_celcius):
     temperature_fahrenheit = (temperature_celcius * 9/5) + 32
     return temperature_fahrenheit
+
+def fahrenheit_to_celcius(temperature_fahrenheit):
+    temperature_celcius = (temperature_fahrenheit - 32) * 5/9
+    return temperature_celcius
