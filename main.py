72160ad4676bfb40647d0ef0ad046bfd59ac4a6e
farrel irawan <irawanssfarrel@gmail.com>
2025-09-27 17:03:54 +0700

    add fahrenheit to kelvin conversion function

--- main.py
+++ main.py
@@ -19,3 +19,7 @@ def fahrenheit_to_celcius(temperature_fahrenheit):
 def fahrenheit_to_reamur(temperature_fahrenheit):
     temperature_reamur = (temperature_fahrenheit - 32) * 4/9
     return temperature_reamur
+
+def fahrenheit_to_kelvin(temperature_fahrenheit):
+    temperature_kelvin = (temperature_fahrenheit - 32) * 5/9 + 273.15
+    return temperature_kelvin
