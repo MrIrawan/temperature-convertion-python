4d9679bd902fd3349d5fa467871a17a41b909ee8
farrel irawan <irawanssfarrel@gmail.com>
2025-09-27 16:58:13 +0700

    add Celsius to Kelvin conversion function

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def celcius_to_reamur(temperature_celcius):
     temperature_reamur = temperature_celcius * 4/5
     return temperature_reamur
 
+def celcius_to_kelvin(temperature_celcius):
+    temperature_kelvin = temperature_celcius + 273.15
+    return temperature_kelvin
+
 def fahrenheit_to_celcius(temperature_fahrenheit):
     temperature_celcius = (temperature_fahrenheit - 32) * 5/9
     return temperature_celcius
